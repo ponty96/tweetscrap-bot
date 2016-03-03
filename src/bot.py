fd800b114a4b5c9217228f864ae30252ef4e7d00
Aregbede Ayomide <ayo.aregbede@gmail.com>
2016-03-03 20:49:01 +0100

    add pubsub mechanism to bot

--- src/bot.py
+++ src/bot.py
@@ -5,6 +5,7 @@ import os
 import sys
 import logging
 from slackclient import SlackClient
+from pubsub import pub
 
 
 
@@ -15,19 +16,33 @@ class Bot(object):
         self.token = token
         self.slack_client = None
         self.replies = {}
+        self.pub = pub
 
     def connect(self):
         self.slack_client = SlackClient(self.token)
         return self.slack_client.rtm_connect()
 
     def run(self):
+        # connect the slack_client and create a websocket connection
         isConnected = self.connect()
-        print(isConnected)
         if isConnected:
             while True:
-                for reply in self.slack_client.rtm_read():
-                    
-                time.sleep(.1)
+                # while True we keep reading from slack every 0.3 secs for messages
+                # for each payload in the array of payloads sent we dispatch a message
+                # which is the type of the payload and the payload itself
+                # any listener gets updated and can perform any action based on the payload
+                for payload in self.slack_client.rtm_read():
+                    sef.dispatchMessage(payload['type'],payload=payload)
+                    print(payload)
+                time.sleep(.3)
         else:
             # should throw an exception here
             print("Connection closed")
+
+    # register a callback to listen to changes
+    def registerListener(self,callback,obj_type):
+        self.pub.subscribe(callback,obj_type)
+
+    # dispatches changes
+    def dispatchMessage(self,obj_type,payload):
+        self.pub.sendMessage(obj_type,payload)
